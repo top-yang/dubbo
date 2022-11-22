b187709f72c7cdadf18c2fccfbea458e98a77b33
yanghao <top.tayloryang@gmail.com>
2022-11-22 16:38:04 +0800

    dubbo如何优雅关闭线程池?

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/ExecutorUtil.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/ExecutorUtil.java
@@ -26,7 +26,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
+/*
+ 线程池五种状态:
+（1）RUNNING：线程池创建之后的初始状态，这种状态下可以执行任务。
+执行shutdown（）
+（2）SHUTDOWN：该状态下线程池不再接受新任务，但是会将工作队列中的任务执行完毕。
+执行shutdownNow（）
+（3）STOP：该状态下线程池不再接受新任务，也不会处理工作队列中的剩余任务，并且将会中断所有工作线程。
+所有任务停止
+（4）TIDYING：该状态下所有任务都已终止或者处理完成，将会执行terminated()钩子方法。
+执行terminated()
+（5）TERMINATED：执行完terminated()钩子方法之后的状态
+ */
 public class ExecutorUtil {
     private static final Logger logger = LoggerFactory.getLogger(ExecutorUtil.class);
     private static final ThreadPoolExecutor shutdownExecutor = new ThreadPoolExecutor(0, 1,
@@ -48,6 +59,9 @@ public class ExecutorUtil {
      *  https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ExecutorService.html
      * @param executor the Executor to shutdown
      * @param timeout the timeout in milliseconds before termination
+     *
+     * dubbo优雅关闭线程池
+     *
      */
     public static void gracefulShutdown(Executor executor, int timeout) {
         if (!(executor instanceof ExecutorService) || isTerminated(executor)) {
@@ -56,6 +70,7 @@ public class ExecutorUtil {
         final ExecutorService es = (ExecutorService) executor;
         try {
             // Disable new tasks from being submitted
+            // shutdown()执行后，不再接收新任务，
             es.shutdown();
         } catch (SecurityException ex2) {
             return;
@@ -64,13 +79,16 @@ public class ExecutorUtil {
         }
         try {
             // Wait a while for existing tasks to terminate
+            // 设置超时等待，等待当前线程池中的任务执行完
             if (!es.awaitTermination(timeout, TimeUnit.MILLISECONDS)) {
+                //shutdownNow()会中断当前的执行任务
                 es.shutdownNow();
             }
         } catch (InterruptedException ex) {
             es.shutdownNow();
             Thread.currentThread().interrupt();
         }
+        //判断线程池是否关闭，若没有，进入循环关闭，补充执行awaitTermination方法
         if (!isTerminated(es)) {
             newThreadToCloseExecutor(es);
         }
@@ -104,6 +122,7 @@ public class ExecutorUtil {
                 @Override
                 public void run() {
                     try {
+                        //循环1000次，每次等待10ms，来判断当前线程池的任务是否全部关闭
                         for (int i = 0; i < 1000; i++) {
                             es.shutdownNow();
                             if (es.awaitTermination(10, TimeUnit.MILLISECONDS)) {
